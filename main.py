7eac64a9603d118aed6cd5573bb47aa1c25426b2
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-20 12:57:28 -0500

    ok

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 class Coche():
-    # constructor 
+    # constructor
     def __init__(self):
-        # __ es para encapsular la propiedad         
-        self.__largoChasis = 250 
+        # __ es para encapsular la propiedad
+        self.largoChasis = 250
         self.__anchoChasis = 120
         self.__ruedas = 4
-        self.enMarcha = False
-        
+        self.enMarcha = True
+
     def arrancar(self):
-     self.enMarcha = True
-     
-    def estado(self):
+        self.enMarcha = True
+
         if (self.enMarcha):
-            return "el coche esta en marcha"
+            chequeo = self.chequeoInterno()
+
+        if (self.enMarcha and chequeo):
+            print("el coche esta en marcha") 
+        elif(self.enMarcha and chequeo==False):
+            print("algo salio mal") 
+
+
+    def chequeoInterno(self):
+        print("realizando chequeo interno")
+        self.gasolina = "ok"
+        self.aceite = "mal"
+        self.puertas = "cerradas"
+
+        if (self.gasolina == "ok" and self.aceite == "ok" and self.puertas == "cerradas"):
+            return True
         else:
-            return "el coche esta detenido"
-              
+            return False
+
+
+
+print("-------------------PARA MI COCHE---------------------")
 miCoche = Coche()
-
-print(f"El largo del coche es : {miCoche.largoChasis}")
+# print(f"El largo del coche es : {miCoche.largoChasis}")
 miCoche.arrancar()
-print(miCoche.estado())
 
+print("------------------PARA TU COCHE-----------------------")
 tuCoche = Coche()
-print(f"El largo del coche es : {miCoche.largoChasis}")
+# print(f"El largo del coche es : {miCoche.largoChasis}")
 miCoche.arrancar()
